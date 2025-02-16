04b3e19cee8aa1d689bdfd4d8231841dd9f18c83
robiulalamdev <robiulalam762400@gmail.com>
2025-02-16 18:13:36 +0600

    all code add for test

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const getNuip = require("nuip");
+
+const app = express();
+const port = 8000;
+
+app.get("/ip", async (req, res) => {
+  try {
+    const ipInfo = await getNuip.getNuip(req);
+    res.json(ipInfo);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch IP info" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
